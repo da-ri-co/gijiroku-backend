431c33a82c391a75a701561e776fe00d934f7e16
ardririy <hynnkgw27@gmail.com>
2024-06-22 15:55:23 +0900

    discussionのリストを返却するAPIモックを描いた

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
-use rocket::{get, launch, routes};
+#[macro_use] extern crate rocket;
 
-#[get("/hc")]
-fn health_check() -> &'static str {
-    "Health OK"
+use rocket_cors::CorsOptions;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DiscussAbstract {
+    id: String, // SQL上のID
+    guild_id: String,
+    start_time: u64,
+    title: String,
+    abst: String,
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![health_check])
+#[get("/all")]
+fn get_all_discussions() -> String {
+    let mock: Vec<DiscussAbstract> =  vec![
+        DiscussAbstract {
+            id: "1".to_string(),
+            guild_id: "1".to_string(),
+            start_time: 1630000000,
+            title: "test1".to_string(),
+            abst: "test1".to_string(),
+        },
+        DiscussAbstract {
+            id: "2".to_string(),
+            guild_id: "2".to_string(),
+            start_time: 1630005000,
+            title: "test2".to_string(),
+            abst: "test2".to_string(),
+        },
+    ];
+
+    serde_json::to_string(&mock).unwrap()
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![index])
+        .mount("/discussions", routes![get_all_discussions])
+        .attach(CorsOptions::default().to_cors().expect("error"))
+        .launch()
+        .await?;
+    Ok(())
 }
